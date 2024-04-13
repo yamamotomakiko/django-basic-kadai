a98043bc4ff3522f955821cfeebf4271745eb6c3
yamamoto-makiko <makiko.315.32@gmail.com>
2024-04-13 20:44:41 +0900

    10kadai

--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
+from django.urls import reverse_lazy
 
 class TopView(TemplateView):
     template_name = "top.html"
@@ -18,3 +19,7 @@ class ProductUpdateView(UpdateView):
     model = Product
     fields = '__all__'
     template_name_suffix = '_update_form'
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('list')
--- myproject/urls.py
+++ myproject/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('crud/', views.ProductListView.as_view(), name="list"),
     path('crud/new/', views.ProductCreateView.as_view(), name="new"),
     path('crud/edit/<int:pk>', views.ProductUpdateView.as_view(), name="edit"),
+    path('crud/delete/<int:pk>', views.ProductDeleteView.as_view(), name="delete"),
 ]
